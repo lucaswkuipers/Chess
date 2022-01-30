ea0532689187a71120f2dde75cd4f8bd6be39472
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2022-01-30 12:26:01 -0300

    Fix valid move state indication

--- Chess/Game/GameBrain.swift
+++ Chess/Game/GameBrain.swift
@@ -18,24 +18,50 @@ final class GameBrain: GameBrainProtocol {
             // First selection: Origin
             if getPiece(from: position) == nil { return }
             origin = position
+
+            // Pawn
+            let originPiece = getPiece(from: origin)
+            if originPiece?.type == .pawn {
+                let validMoves = getValidPawnMoves(from: position)
+
+                for validMove in validMoves {
+                    print("set valid for ROW: \(validMove.row), COL: \(validMove.column)")
+                    setState(.valid, to: validMove)
+                }
+            }
         } else {
             // Second selection: Destination
 
             // Validation
             let destinationPiece = getPiece(from: position)
             let originPiece = getPiece(from: origin)
+
             if destinationPiece?.color == originPiece?.color {
                 origin = position
+                resetStates()
+
+                // Pawn
+                let originPiece = getPiece(from: origin)
+                if originPiece?.type == .pawn {
+                    let validMoves = getValidPawnMoves(from: position)
+
+                    for validMove in validMoves {
+                        print("set valid for ROW: \(validMove.row), COL: \(validMove.column)")
+                        setState(.valid, to: validMove)
+                    }
                 return
+                }
             }
 
             // Apply changes
+            resetStates()
             destination = position
             setPiece(getPiece(from: origin), to: destination)
             cleanPiece(from: origin)
+            resetStates()
             origin = nil
             destination = nil
-            printBoard()
+            printBoard() // log purposes (visualize what should be happening)
         }
     }
 
@@ -47,6 +73,17 @@ final class GameBrain: GameBrainProtocol {
 
         board[position.row][position.column].piece = piece
         delegate?.setBoard(to: board)
+    }
+
+    private func setState(_ state: SpotState, to position: Position?) {
+        guard let position = position else { return }
+
+        if position.row >= board.count { return }
+        if position.column >= board[position.row].count { return }
+
+        board[position.row][position.column].spotState = state
+        delegate?.setBoard(to: board)
+
     }
 
     private func getPiece(from position: Position?) -> Piece? {
@@ -66,6 +103,50 @@ final class GameBrain: GameBrainProtocol {
                 line.append("[\(pieceName)]")
             }
             print(line)
+        }
+    }
+
+    // MARK: - Pieces
+
+    private func getValidPawnMoves(from position: Position) -> [Position] {
+        let color = getPiece(from: position)?.color
+        var validMoves: [Position] = []
+
+        // White
+        if color == .white {
+            // Initial two moves
+            if position.row == 6 {
+                let move = Position(row: 4, column: position.column)
+                if getPiece(from: move) == nil {
+                    validMoves.append(move) // two moves up
+                }
+            }
+
+            // Normal front
+            let move = Position(row: position.row - 1, column: position.column)
+            if getPiece(from: move) == nil {
+                validMoves.append(move)
+            }
+
+            // Capture (diagonals)
+            let leftCaptureMove = Position(row: position.row - 1, column: position.column - 1)
+            if getPiece(from: leftCaptureMove)?.color == .black {
+                validMoves.append(leftCaptureMove)
+            }
+
+            let rightCaptureMove = Position(row: position.row - 1, column: position.column + 1)
+            if getPiece(from: rightCaptureMove)?.color == .black {
+                validMoves.append(rightCaptureMove)
+            }
+        }
+        return validMoves
+    }
+
+    private func resetStates() {
+        for (rowIndex, row) in board.enumerated() {
+            for (spotIndex, _) in row.enumerated() {
+                board[rowIndex][spotIndex].spotState = .default
+            }
         }
     }
 }
